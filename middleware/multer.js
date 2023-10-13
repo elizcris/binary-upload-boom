e7699d9ffc7ed3a9fb05062c9c3f21554c695d05
Elizabeth Cristina <lizzy.cristina@gmail.com>
2023-10-13 05:46:27 -0500

    Comment multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 module.exports = multer({
   storage: multer.diskStorage({}),
+  //we are checking the file type
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
     if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
